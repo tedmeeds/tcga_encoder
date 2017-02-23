b639fa1511665f8edf5da83b06e3d4646d524bee
Ted Meeds <tmeeds@gmail.com>
2017-02-23 22:56:36 +0100

    adjust

--- tcga_encoder/models/survival/main.py
+++ tcga_encoder/models/survival/main.py
@@ -149,7 +149,7 @@ def main(yaml_file, weights_matrix):
       
       survival_target = -mn_proj
       C = 1.0
-      C_reg = 0.045
+      C_reg = 0.00145
       n2show = 10
       reg_rna_class_projections, \
       reg_rna_class_weights, \
