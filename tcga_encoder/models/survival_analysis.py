5b5bab7a129ee82fada785f7ff1dda1f6de7b602
Ted Meeds <tmeeds@gmail.com>
2017-03-13 14:36:48 +0100

    adjustments

--- tcga_encoder/models/survival_analysis.py
+++ tcga_encoder/models/survival_analysis.py
@@ -505,7 +505,7 @@ def predict_groups_with_xval_with_regression( X_orig, y_orig, l1, k_fold=10, ran
                min_epochs = 2000, \
                logging_frequency = 500, \
                testing_frequency = 500, \
-               lr=0.001, l1=l1 ,l2=0.00 ) #n_epochs=2000, lr = 0.01, logging_frequency = 500 )
+               lr=0.0025, l1=l1 ,l2=0.00 ) #n_epochs=2000, lr = 0.01, logging_frequency = 500 )
     #sk_lda = sklearn.linear_model.Lasso(alpha=l1, fit_intercept=True, normalize=True)
     #sk_lda.fit( X_train, y_train )
     
