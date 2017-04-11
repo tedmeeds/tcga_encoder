90cf52a6e6cbac8da8e6fe8e7dab214a249607e4
Ted Meeds <tmeeds@gmail.com>
2017-04-11 14:14:29 +0200

    fix

--- tcga_encoder/models/vae/batcher_dna_prediction_from_sources.py
+++ tcga_encoder/models/vae/batcher_dna_prediction_from_sources.py
@@ -346,7 +346,7 @@ class DnaBatcher( TCGABatcherABC ):
       ok = np.zeros( x_obs.shape[1] )
       for d_idx in xrange( x_obs.shape[1] ):
         
-        if y_obs[:,d_idx].sum()>0 and y_obs[:,d_idx].sum() ~= 0:
+        if y_obs[:,d_idx].sum()>0 and y_obs[:,d_idx].sum() != 0:
           try:
             auc[d_idx] = roc_auc_score(y_obs[:,d_idx],x_obs[:,d_idx])
           except:
