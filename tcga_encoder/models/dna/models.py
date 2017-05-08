8aba0f0ef1673acbfda0b052febc596a157e107d
Ted Meeds <tmeeds@gmail.com>
2017-05-08 16:15:38 +0200

    common var

--- tcga_encoder/models/dna/models.py
+++ tcga_encoder/models/dna/models.py
@@ -127,8 +127,10 @@ class GaussianNaiveBayesModel( object ):
     self.mu_1 = X[self.class_1,:].mean(0)
     self.mu_0 = X[self.class_0,:].mean(0)
     self.var_1 = X[self.class_1,:].var(0) 
-    self.var_0 = X[self.class_0,:].var(0) 
+    #self.var_0 = X[self.class_0,:].var(0) 
+    self.var_0 = X.var(0) 
     
+    self.var_1 = self.var_0
     self.w = self.mu_1/self.var_1 - self.mu_0/self.var_0
     self.b_vec = self.mu_0*self.mu_0/(2*self.var_0) - self.mu_1*self.mu_1/(2*self.var_1) + 0.5*np.log(self.var_0)- 0.5*np.log(self.var_1)          
 
