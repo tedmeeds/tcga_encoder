a1e743d0d0c7c558f2bcb2bd7c2896975eb33adf
Ted Meeds <tmeeds@gmail.com>
2017-05-12 17:30:46 +0200

    multiples

---
+++ tcga_encoder/models/dna/naive_bayes_all.py
@@ -0,0 +1,73 @@
+from tcga_encoder.utils.helpers import *
+from tcga_encoder.data.data import *
+from tcga_encoder.definitions.tcga import *
+from tcga_encoder.definitions.nn import *
+from tcga_encoder.definitions.locations import *
+from sklearn.model_selection import StratifiedKFold
+from scipy import stats
+from tcga_encoder.models.dna.models import *
+from tcga_encoder.models.dna.naive_bayes_by_gene import main
+
+disease_groups = [
+  ["acc"],
+  ["blca"],
+  ["brca"],
+  ["cesc"],
+  ["chol"],
+  ["coad" ,"read"],
+  ["dlbc"],
+  ["esca"],
+  ["gbm","lgg"],
+  ["hnsc"],
+  ["kich","kirc","kirp"],
+  ["lihc"],
+  ["luad"],
+  ["lusc"],
+  ["ov"],
+  ["paad"],
+  ["pcpg"],
+  ["prad"],
+  ["sarc"],
+  ["skcm"],
+  ["stad"],
+  ["tgct"],
+  ["thca"],
+  ["thym"],
+  ["ucec"],
+  ["ucs"],
+  ["uvm"]
+]
+
+if __name__ == "__main__":
+  #assert len(sys.argv) >= 2, "Must pass yaml file."
+  data_file        = sys.argv[1]
+  results_location = sys.argv[2]
+  dna_gene         = sys.argv[3]
+  source           = sys.argv[4]
+  method           = sys.argv[5]
+  
+  
+  n_folds        = 4   # nbr of folds per xval repeat
+  n_xval_repeats = 5   # nbr of xval permutations/repeats to try
+  n_permutations = 10  # nbr of random label assignments to try
+  
+  if len(sys.argv) >= 9:
+    n_folds        = int( sys.argv[6] )
+    n_xval_repeats = int( sys.argv[7] )
+    n_permutations = int( sys.argv[8] )
+    
+  #train = False
+  #if len(sys.argv) == 10:
+  train = True #bool(int( sys.argv[9]))
+    
+  restricted_diseases = []
+  idx = 10
+  while len(sys.argv) > idx:
+    restricted_diseases.append( sys.argv[idx] )
+    idx += 1
+  
+  dna_gene = "APC"
+  for restricted_diseases in disease_groups:
+    print "running ", dna_gene, " on ", restricted_diseases 
+    #main( data_file, results_location, dna_gene, source, method, n_folds, n_xval_repeats, n_permutations, train, restricted_diseases )
+  #pdb.set_trace()
