b573be75e90042011028dc5f3ade93fce31e072f
Ted Meeds <tmeeds@gmail.com>
2017-05-12 17:36:09 +0200

    get genes

--- tcga_encoder/models/dna/naive_bayes_all.py
+++ tcga_encoder/models/dna/naive_bayes_all.py
@@ -42,32 +42,42 @@ if __name__ == "__main__":
   #assert len(sys.argv) >= 2, "Must pass yaml file."
   data_file        = sys.argv[1]
   results_location = sys.argv[2]
-  dna_gene         = sys.argv[3]
-  source           = sys.argv[4]
-  method           = sys.argv[5]
+  #dna_gene         = sys.argv[3]
+  source           = sys.argv[3]
+  method           = sys.argv[4]
   
   
   n_folds        = 4   # nbr of folds per xval repeat
   n_xval_repeats = 5   # nbr of xval permutations/repeats to try
   n_permutations = 10  # nbr of random label assignments to try
   
-  if len(sys.argv) >= 9:
-    n_folds        = int( sys.argv[6] )
-    n_xval_repeats = int( sys.argv[7] )
-    n_permutations = int( sys.argv[8] )
+  if len(sys.argv) >= 8:
+    n_folds        = int( sys.argv[5] )
+    n_xval_repeats = int( sys.argv[6] )
+    n_permutations = int( sys.argv[7] )
     
   #train = False
   #if len(sys.argv) == 10:
   train = True #bool(int( sys.argv[9]))
     
-  restricted_diseases = []
-  idx = 10
-  while len(sys.argv) > idx:
-    restricted_diseases.append( sys.argv[idx] )
-    idx += 1
+  data_store = pd.HDFStore( data_file, "r" )
+  sources = [DNA, source]
+  observed = data_store["/CLINICAL/observed"][ sources ] 
+  barcodes = observed[ observed.sum(1)==len(sources) ].index.values
+  
+  variant = "Missense_Mutation"
+  dna_data    = data_store["/DNA/channel/0"].loc[ barcodes ]
+  data_store.close()
+  dna_genes = dna_data.columns
+  # restricted_diseases = []
+  # idx = 10
+  # while len(sys.argv) > idx:
+  #   restricted_diseases.append( sys.argv[idx] )
+  #   idx += 1
   
   dna_gene = "APC"
-  for restricted_diseases in disease_groups:
-    print "running ", dna_gene, " on ", restricted_diseases 
+  for dna_gene in dna_genes[:10]:
+    for restricted_diseases in disease_groups:
+      print "running ", dna_gene, " on ", restricted_diseases 
     #main( data_file, results_location, dna_gene, source, method, n_folds, n_xval_repeats, n_permutations, train, restricted_diseases )
   #pdb.set_trace()
