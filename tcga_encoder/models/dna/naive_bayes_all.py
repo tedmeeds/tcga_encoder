f61de0894ec756f536267abb65ce7f11bee14331
Ted Meeds <tmeeds@gmail.com>
2017-05-12 17:41:49 +0200

    add in the actual training

--- tcga_encoder/models/dna/naive_bayes_all.py
+++ tcga_encoder/models/dna/naive_bayes_all.py
@@ -81,6 +81,6 @@ if __name__ == "__main__":
       print "-----------------------------------------------------------------"
       print "running ", dna_gene, " on ", restricted_diseases 
       print "-----------------------------------------------------------------"
-      #main( data_file, results_location, dna_gene, source, method, n_folds, n_xval_repeats, n_permutations, train, restricted_diseases )
+      main( data_file, results_location, dna_gene, source, method, n_folds, n_xval_repeats, n_permutations, train, restricted_diseases )
     #main( data_file, results_location, dna_gene, source, method, n_folds, n_xval_repeats, n_permutations, train, restricted_diseases )
   #pdb.set_trace()
