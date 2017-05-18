57ebd09a9c8a6cf5823388e614787150048c5e3c
Ted Meeds <tmeeds@gmail.com>
2017-05-18 16:05:35 +0200

    debugging

--- tcga_encoder/data/create_data_may_2017.py
+++ tcga_encoder/data/create_data_may_2017.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
   print "SAVE:",save_location
   dataset = MultiSourceData( save_location )
   broad_location = os.path.join( os.environ.get('HOME','/'), y["broad_location"] )
-
+  broad_analyses_location = os.path.join( os.environ.get('HOME','/'), y["broad_analyses_location"] )
   # add CLINICAL first
   print "Loading CLINICAL"
   clinical_h5 =ReadH5( os.path.join( broad_location, y["sources"][CLINICAL]["data_store"]) )
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         min_nbr_in_pan = source_spec["min_nbr_in_pan"]
       dataset.AddDNA( broad_location, source_name, dna_h5, dna_h5_raw, mutation_channels=mutation_channels, genes2keep=dna_genes, min_nbr_in_pan=min_nbr_in_pan )
     elif source_name == RNA:
-      rna_h5 = ReadH5( os.path.join( broad_location, source_spec["data_store"]) )
+      rna_h5 = ReadH5( os.path.join( broad_analyses_location, source_spec["data_store"]) )
       #print "loading ", source_spec["data_store_ga"]
       #rna_h5_ga = ReadH5( os.path.join( broad_location, source_spec["data_store_ga"]) )
       #print "loading ", source_spec["data_store_hi"]
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     elif source_name == miRNA:
       #mirna_h5_ga = ReadH5( os.path.join( broad_location, source_spec["data_store_ga"]) )
       #mirna_h5_hi = ReadH5( os.path.join( broad_location, source_spec["data_store_hi"]) )
-      mirna_h5 = ReadH5( os.path.join( broad_location, source_spec["data_store"]) )
+      mirna_h5 = ReadH5( os.path.join( broad_analyses_location, source_spec["data_store"]) )
       #mirna_h5_hi = ReadH5( os.path.join( broad_location, source_spec["data_store_hi"]) )
       nbr = source_spec["nbr"]
       method = source_spec["method"]
@@ -101,7 +101,7 @@ if __name__ == "__main__":
       self.store[ miRNA + "/" + "FAIR" + "/" ] = mirna_h5[miRNA + "/" + "FAIR" + "/"][ genes2keep ]
       
     elif source_name == METH:
-      meth_h5 = ReadH5( os.path.join( broad_location, source_spec["data_store"]) )
+      meth_h5 = ReadH5( os.path.join( broad_analyses_location, source_spec["data_store"]) )
       nbr = source_spec["nbr"]
       method = source_spec["method"]
       
