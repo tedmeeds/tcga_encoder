69e70ef41e7c060e5abcf138f9a8aea62b5a9ccf
Ted Meeds <tmeeds@gmail.com>
2017-06-13 16:42:14 +0200

    debug

--- tcga_encoder/data/data.py
+++ tcga_encoder/data/data.py
@@ -542,15 +542,16 @@ class MultiSourceData(object):
     
     print "** METH filter for tumor samples only"
     patient_disease = h5["admin.disease_code"].values
-    patient_bcs     = h5["patient.bcr_patient_barcode"].values
-    pdb.set_trace()
+    patient_bcs     = h5.index.values
+    patient_rows     = h5["patient.bcr_patient_barcode"].values
+    #pdb.set_trace()
     #patient_rows    = h5["Methpatient.bcr_patient_barcode"].values
     
     keep_bcs        = []
     keep_query      = []
     last_bc         = None
-    for disease,bc in zip(patient_disease,patient_bcs):
-      pbc = bc[:12]
+    for disease,pbc,bc in zip(patient_disease,patient_rows,patient_bcs):
+      #pbc = bc[:12]
       sample_type = bc[13:15]
       if (    sample_type == '01' \
            or sample_type == '02' \
