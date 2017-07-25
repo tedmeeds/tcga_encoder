9e295afef1cc1f9129eab6b0a4160d52057529ab
Ted Meeds <tmeeds@gmail.com>
2017-07-25 17:45:27 +0200

    more k

--- tcga_encoder/analyses/everything.py
+++ tcga_encoder/analyses/everything.py
@@ -2798,7 +2798,7 @@ if __name__ == "__main__":
   #spearmanr_latent_space_by_inputs(data, force=True)
   ridges = [0.00001, 0.001,1.0]
   
-  deeper_meaning_dna_and_z_correct( data, K=5, min_p_value=0.25, threshold=0, Cs = [0.00001, 0.001,0.1,10.0,1000.0] )
+  deeper_meaning_dna_and_z_correct( data, K=10, min_p_value=0.25, threshold=0, Cs = [0.00001,0.0001, 0.001,0.1,1.0,10.0,1000.0] )
   #deeper_meaning_dna_and_z( data, min_p_value=1e-2, threshold=0, ridges = [0.00001, 0.001,0.1,10.0,1000.0] )
   #deeper_meaning_dna_and_z( data, min_p_value=1e-3, threshold=0, ridges = [0.00001, 0.001,0.1,10.0,1000.0] )
   #deeper_meaning_dna_and_z( data, min_p_value=1e-4, threshold=0, ridges = [0.00001, 0.001,0.1,10.0,1000.0] )
