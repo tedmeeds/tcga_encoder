4a73656013daf06c97d31b92dc979bced9a0d88a
Ted Meeds <tmeeds@gmail.com>
2017-08-16 15:10:38 +0200

    ds

--- tcga_encoder/analyses/everything_survival.py
+++ tcga_encoder/analyses/everything_survival.py
@@ -3468,11 +3468,11 @@ if __name__ == "__main__":
   
   for K in [2,5]:
     #K=2
-    L2s_Z = [0.1,1.0]
+    L2s_Z = [0.1]
     L2s_RNA = [0.001,0.01,0.1,1.0]
     #survival_regression_global( data, data.Z, "Z", L2s_Z, K = K, repeats=20, fitter = CoxPHFitter  )
-    survival_regression_global( data, data.Z, "Z", L2s_Z, K = K, repeats=10, fitter = CoxPHFitter  )
-    survival_regression_global_g0_v_g3( data, data.Z, "Z", L2s_Z, K = K, repeats=10, fitter = CoxPHFitter  )
+    survival_regression_global( data, data.Z, "Z", L2s_Z, K = K, repeats=20, fitter = CoxPHFitter  )
+    survival_regression_global_g0_v_g3( data, data.Z, "Z", L2s_Z, K = K, repeats=20, fitter = CoxPHFitter  )
     #survival_regression_global( data, data.RNA_scale, "RNA_scale", L2s_RNA, K = K, repeats=5, fitter = CoxPHFitter  )
     #survival_regression_global( data, data.RNA_fair, "RNA_fair", L2s_RNA, K = K, repeats=5, fitter = CoxPHFitter  )
   
