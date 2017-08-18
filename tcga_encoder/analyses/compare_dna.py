4694e4a93614679771fc5fe3b0065812f430fd4f
Ted Meeds <tmeeds@gmail.com>
2017-08-18 10:33:28 +0200

    weg

--- tcga_encoder/analyses/compare_dna.py
+++ tcga_encoder/analyses/compare_dna.py
@@ -32,7 +32,7 @@ from scipy import stats
 if __name__ == "__main__":
   
   name1 = "RNA_scale"
-  name2 = "Z 200"
+  name2 = "Z 500"
   #dir_1_short = "results/tcga_vae_post_recomb9/xlarge/xval_rec_not_blind_fix_outliers/20_z_100_h_1000_anti_5000/fold_1_of_50/everything/correct_deeper_meaning_dna_and_rna_fair_tissue_0.05_p_spear_1_logreg"
   #dir_2_short = "results/tcga_vae_post_recomb9/xlarge/xval_rec_not_blind_fix_outliers/20_z_100_h_1000_anti_5000/fold_1_of_50/everything/correct_deeper_meaning_dna_and_z_p_tissue_0.05_p_spear_1_logreg"
   dir_1_short = "results/tcga_vae_post_recomb9/large/xval_rec_not_blind_fix_outliers/22_z_200_h_2000_anti_5000/fold_1_of_50/everything2/correct_pancancer_dna_and_RNA_scale_pan_min_2000_max_2000_0.00_p_spear_1_logreg"
--- tcga_encoder/analyses/everything.py
+++ tcga_encoder/analyses/everything.py
@@ -3253,17 +3253,17 @@ if __name__ == "__main__":
   # #deeper_meaning_dna_and_z_correct( data, K=10, min_p_value=1e-3, threshold=0, Cs = [0.00001,0.0001, 0.001,0.1,1.0,10.0,1000.0] )
   nbr_dna_genes2process = 100
   Cs = [0.00001,0.0001, 0.001,0.01,0.1,1.0,10.0,100.0,1000.0]
-  Cs = [0.00001, 0.001,0.1,10.0,1000.0]
+  Cs = [0.0,0.00001, 0.001,0.1,10.0,1000.0]
   #
   K=4
   min_p_value=1.0
-  threshold=0.0
+  threshold=0.05
   min_features = data.Z.values.shape[1]
-  #deeper_meaning_dna_and_data_correct_pan( data, data.Z, "Z", min_features=min_features,max_features=min_features, nbr_dna_genes2process=nbr_dna_genes2process,K=K, min_p_value=min_p_value, threshold=threshold, Cs = Cs )
+  deeper_meaning_dna_and_data_correct_pan( data, data.Z, "Z", min_features=min_features,max_features=min_features, nbr_dna_genes2process=nbr_dna_genes2process,K=K, min_p_value=min_p_value, threshold=threshold, Cs = Cs )
   #
   min_features = data.RNA_fair.values.shape[1]
   deeper_meaning_dna_and_data_correct_pan( data, tanh(data.RNA_scale), "RNA_scale", min_features=min_features,max_features=min_features,nbr_dna_genes2process=nbr_dna_genes2process,K=K, min_p_value=min_p_value, threshold=threshold, Cs = Cs )
-  #deeper_meaning_dna_and_data_correct_pan( data, data.RNA_fair, "RNA_fair", min_features=min_features,max_features=min_features, nbr_dna_genes2process=nbr_dna_genes2process,K=K, min_p_value=min_p_value, threshold=threshold, Cs = Cs )
+  deeper_meaning_dna_and_data_correct_pan( data, data.RNA_fair, "RNA_fair", min_features=min_features,max_features=min_features, nbr_dna_genes2process=nbr_dna_genes2process,K=K, min_p_value=min_p_value, threshold=threshold, Cs = Cs )
   #
   # threshold=0.05
   # min_features = data.Z.values.shape[1]
